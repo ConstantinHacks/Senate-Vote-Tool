f862de8c00067962b1062dc6e0e40010358840d9
Constantin Koehler <constantinfakoehler@gmail.com>
2018-01-31 01:21:50 -0500

    Delete Table UI Added functioning networking

--- src/index.js
+++ src/index.js
@@ -1,104 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 
-const SENATORS = [{
-    name: "Jeff Flake",
-    party: "R",
-    id: 1
-  }, {
-    name: "Chuck Schumer",
-    party: "D",
-    id: 2
-  }, {
-    name: "Bernie Sanders",
-    party: "D",
-    id: 3
-  }];
-
-  class SenatorRow extends React.Component {
-    render() {
-      const senator = this.props.senator;
-  
-      return (
-        <tr>
-          <td>{senator.name}</td>
-        </tr>
-      );
-    }
-  }
 
 class App extends React.Component {
-    render(){
-        const dem_rows = [];
-        const gop_rows = [];
-
-        console.log(this.props.senators);
-        
-
-        this.props.senators.forEach((senator) => {
-            if(senator.party === "D"){
-                dem_rows.push(
-                    <SenatorRow key={senator.id} senator={senator}/>
-                )
-            } else {
-                gop_rows.push(
-                    <SenatorRow key={senator.id} senator={senator}/>
-                )
-            }
-        });
-
-        return (
-             <div className="row">
-                <div className="col-sm">
-                    <table className="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="demsNo">Democrats (Nay)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {dem_rows}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="col-sm">
-                    <table className="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="demsNo">Democrats (Nay)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
-                </div>
-                <div className="col-sm">
-                    <table className="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th id="GOPYes" scope="col">Republicans (Yea)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
-                </div>
-                <div className="col-sm">
-                    <table className="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th id="GOPNo" scope="col">Republicans (Nay)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {gop_rows}
-                            </tbody>
-                        </table>
-                </div>
-            </div>
-        );
+    componentDidMount(){
+        axios.get('https://theunitedstates.io/congress-legislators/legislators-current.json')
+            .then(res => {                
+                const senators = res.data.filter(legislator => legislator.terms[legislator.terms.length-1].type === "sen");
+                return senators
+            });
     }
+
+    render(){
+        return(
+        <div className="row">
+            <div className="col-sm">
+                Democrats voting Nay
+            </div>
+            <div className="col-sm">
+                Democrats voting Yay
+            </div>
+            <div className="col-sm">
+                Republicans voting Yay
+            </div>
+            <div className="col-sm">
+                Republicans voting Nay
+            </div>
+        </div>
+        )
+    };
 }
 
-ReactDOM.render(<App senators={SENATORS}/>,document.getElementById("root"))
+
+
+ReactDOM.render(<App/>,document.getElementById("root"))
